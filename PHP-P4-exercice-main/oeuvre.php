475d244137bce3b50c6b7280ea120d8230275e3d
Bennour Mehdi <bennourmehdi@air-de-bennour.home>
2026-06-18 23:46:48 +0200

    Affichage dynamique de la page detail oeuvre + suppression oeuvres.php

--- PHP-P4-exercice-main/oeuvre.php
+++ PHP-P4-exercice-main/oeuvre.php
@@ -1,40 +1,34 @@
 <?php
-    require 'header.php';
-    require 'oeuvres.php';
+require('header.php');
+require('bdd.php');
 
-    // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
-    if(empty($_GET['id'])) {
-        header('Location: index.php');
-    }
+if(empty($_GET['id'])) {
+    header('Location: index.php');
+    exit;
+}
 
-    $oeuvre = null;
+$bdd = connexion();
+$requete = $bdd->prepare('SELECT * FROM oeuvres WHERE id = :id');
+$requete->execute(['id' => $_GET['id']]);
+$oeuvre = $requete->fetch();
 
-    // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
-        // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
-            break; // On stoppe le foreach si on a trouvé l'oeuvre
-        }
-    }
-
-    // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
-    if(is_null($oeuvre)) {
-        header('Location: index.php');
-    }
+if($oeuvre === false) {
+    header('Location: index.php');
+    exit;
+}
 ?>
 
 <article id="detail-oeuvre">
     <div id="img-oeuvre">
-        <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
+        <img src="<?php echo $oeuvre['image']; ?>" alt="<?php echo $oeuvre['titre']; ?>">
     </div>
     <div id="contenu-oeuvre">
-        <h1><?= $oeuvre['titre'] ?></h1>
-        <p class="description"><?= $oeuvre['artiste'] ?></p>
+        <h1><?php echo $oeuvre['titre']; ?></h1>
+        <p class="description"><?php echo $oeuvre['artiste']; ?></p>
         <p class="description-complete">
-             <?= $oeuvre['description'] ?>
+            <?php echo $oeuvre['description']; ?>
         </p>
     </div>
 </article>
 
-<?php require 'footer.php'; ?>
+<?php require('footer.php'); ?>
